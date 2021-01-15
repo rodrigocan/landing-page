ee72223f739e105ae029c453d07d86ce17d37fe6
Rodrigo Candido <rodrigo@DESKTOP-U09JB93.localdomain>
2021-01-15 12:14:45 -0300

    Final da aula 79

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -71,6 +71,19 @@ const GET_LANDING_AGE = /* GraphQL */ `
     }
   }
 
+  fragment pricingBox on LandingPage {
+    pricingBox {
+      totalPrice
+      numberInstallments
+      priceInstallment
+      benefits
+      button {
+        label
+        url
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
       ...logo
@@ -80,6 +93,7 @@ const GET_LANDING_AGE = /* GraphQL */ `
       ...sectionConcepts
       ...sectionModules
       ...sectionAgenda
+      ...pricingBox
     }
   }
 `
